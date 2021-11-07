0e37894ce6ebfb868e4a58ab8679bbd46f272b5a
Chris Kraemer <kraemer@u.northwestern.edu>
2021-11-07 14:16:02 -0600

    fixed test.ts

--- test.ts
+++ test.ts
@@ -1,42 +1,44 @@
 // Name tag
-inkybit.drawRectangle(0, 0, 249, 121, inkybit.Color.Accent, true)
-inkybit.drawRectangle(0, 30, 249, 60, inkybit.Color.White, true)
+inkyfixed.drawRectangle(0, 0, 249, 121, inkyfixed.Color.Accent, true)
+inkyfixed.drawRectangle(0, 30, 249, 60, inkyfixed.Color.White, true)
 
-inkybit.drawText("Hello, my name is:", 10, 10, inkybit.Color.White, inkybit.TextSize.Regular)
+//inkyfixed.drawText("Hello, my name is:", 10, 10, inkyfixed.Color.White, inkyfixed.TextSize.Regular)
 
-let size: number = inkybit.measureText("Phillius", inkybit.TextSize.Large)
-let x: number = Math.floor((250 - size) / 2)
-inkybit.drawText("Phillius", x, 50, inkybit.Color.Black, inkybit.TextSize.Large)
+//let size: number = inkyfixed.measureText("Phillius", inkyfixed.TextSize.Large)
+//let x: number = Math.floor((250 - size) / 2)
+//inkyfixed.drawText("Phillius", x, 50, inkyfixed.Color.Black, inkyfixed.TextSize.Large)
 
 // Lines
-inkybit.drawLine(0, 0, 50, 50, inkybit.Color.Accent)
-inkybit.drawLine(50, 0, 0, 50, inkybit.Color.Black)
-inkybit.drawLine(0, 0, 0, 50, inkybit.Color.Black)
+inkyfixed.drawLine(0, 0, 50, 50, inkyfixed.Color.Accent)
+inkyfixed.drawLine(50, 0, 0, 50, inkyfixed.Color.Black)
+inkyfixed.drawLine(0, 0, 0, 50, inkyfixed.Color.Black)
 
-inkybit.drawRectangle(55, 5, 50, 50, inkybit.Color.Accent, false)
-inkybit.drawRectangle(110, 5, 50, 50, inkybit.Color.Black, true)
+inkyfixed.drawRectangle(55, 5, 50, 50, inkyfixed.Color.Accent, false)
+inkyfixed.drawRectangle(110, 5, 50, 50, inkyfixed.Color.Black, true)
 
 // Variable pixel size
 let n:number = 0;
 for (n = 0; n < 50; n++){
-    inkybit.setPixel(n, n, 1)
+    inkyfixed.setpixel(n, n, 1)
 }
 for (n = 0; n < 50; n++){
-    inkybit.setPixel(n, 50-n, 1)
+    inkyfixed.setpixel(n, 50-n, 1)
 }
 
-inkybit.setPixelSize(inkybit.PixelSize.Normal)
-inkybit.drawText("Ahoy", 1, 1, inkybit.Color.Black, inkybit.TextSize.Regular)
+inkyfixed.setPixelSize(inkyfixed.PixelSize.Normal)
+//inkyfixed.drawText("Ahoy", 1, 1, inkyfixed.Color.Black, inkyfixed.TextSize.Regular)
 
 // Icon tokenization
-inkybit.drawText("Hello {Asleep}", 1, 22)
+//inkyfixed.drawText("Hello {Asleep}", 1, 22)
 
 // Icons
-inkybit.drawIcon(IconNames.Heart, 0, 1, inkybit.Color.Accent, inkybit.TextSize.Tiny)
-inkybit.drawIcon(IconNames.Heart, 0, 6, inkybit.Color.Accent, inkybit.TextSize.Regular)
-inkybit.drawIcon(IconNames.Heart, 0, 41, inkybit.Color.Accent, inkybit.TextSize.Medium)
-inkybit.drawIcon(IconNames.Heart, 41, 0, inkybit.Color.Accent, inkybit.TextSize.Large)
+/*
+inkyfixed.drawIcon(IconNames.Heart, 0, 1, inkyfixed.Color.Accent, inkyfixed.TextSize.Tiny)
+inkyfixed.drawIcon(IconNames.Heart, 0, 6, inkyfixed.Color.Accent, inkyfixed.TextSize.Regular)
+inkyfixed.drawIcon(IconNames.Heart, 0, 41, inkyfixed.Color.Accent, inkyfixed.TextSize.Medium)
+inkyfixed.drawIcon(IconNames.Heart, 41, 0, inkyfixed.Color.Accent, inkyfixed.TextSize.Large)
+*/
 
-inkybit.show()
+inkyfixed.show()
 
-inkybit.clear()
+inkyfixed.clear()
